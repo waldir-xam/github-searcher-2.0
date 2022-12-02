3cfa8dd07c6b47acb5f9e5b4471a900d94ff4796
Waldir <waliuxd@gmail.com>
2022-12-01 20:56:34 -0500

    next

--- src/pages/user/user.js
+++ src/pages/user/user.js
@@ -22,7 +22,7 @@ const User = () => {
   const fetchUser = async () => {
     const data = await searchUser(username);
     setUserDetail(data);
-    console.log(data);
+    console.log("detail", data);
   };
 
   const fetchRepos = async () => {
@@ -31,6 +31,7 @@ const User = () => {
     console.log("repos", data);
   };
 
+  /*PARA "EXPORTAR " EL FETCH DE LAS FUNCIONES*/
   useEffect(() => {
     fetchUser();
   }, []);
@@ -40,61 +41,90 @@ const User = () => {
   }, []);
 
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="md" style={{
+      backgroundColor: "#28336f", color: "#fff", borderRadius:"1em"
+    }}>
       {userDetail && (
         <Box mt={10}>
           <Grid container spacing={3}>
+
+            {/*DETALLES DE USUARIO */}
+
             <Grid item xs={12} md={4}>
-              <Box mb={1}>
+              <Box mb={1} style={{
+                textAlign: "center",
+              }}>
                 <img
                   style={{
-                    borderRadius: "50%",
+                    borderRadius: "0%",
                   }}
                   width={200}
                   src={userDetail.avatar_url}
                   alt={userDetail.login}
                 />
               </Box>
-              <Typography mb={1} variant="h6">
+              {/* NAME */}
+              <Typography mb={1} variant="h6" style={{
+                textAlign: "center", fontWeight: "600"
+              }}>
                 {userDetail.name}
               </Typography>
-              <Typography mb={3} variant="h6" fontWeight="350" color="#BBBBBB">
+
+              {/* USUARIO */}
+              <Typography mb={3} variant="h6" fontWeight="600" color="#B2B2B2"
+                style={{
+                  textAlign: "center"
+                }}>
                 {userDetail.login}
               </Typography>
+
+              {/* BIO */}
               <Typography mb={2} variant="body1">
                 {userDetail.bio}
               </Typography>
-              <Button variant="contained" fullWidth href={`${userDetail.html_url}`}>
-                Ver Perfil en Github
-              </Button>
-              <TypographyIconBio
-                icon="people"
-                text={`${userDetail.followers} followers . ${userDetail.following} following`}
-              />
-              <TypographyIconBio icon="company" text={userDetail.company} />
-              <TypographyIconBio icon="location" text={userDetail.location} />
 
-              {userDetail.email && (
+              <CardContent style={{
+                backgroundColor: "#000", color: "#fff", borderRadius: "1em"
+              }}>
+                {/* BOTON VER PERFIL */}
+                <Button variant="contained" fullWidth href={`${userDetail.html_url}`}>
+                  Ver Perfil en Github
+                </Button>
+
+                {/* ICONOS DETALLE BIO */}
+                <TypographyIconBio
+                  icon="people"
+                  text={`${userDetail.followers} followers . ${userDetail.following} following`}
+                />
+                <TypographyIconBio icon="company" text={userDetail.company} />
+                <TypographyIconBio icon="location" text={userDetail.location} />
                 <TypographyIconBio icon="email" text={userDetail.email} />
-              )}
-              <TypographyIconBio icon="blog" text={userDetail.blog} />
-              <TypographyIconBio
-                icon="social"
-                text={userDetail.twitter_username}
-              />
+                <TypographyIconBio icon="blog" text={userDetail.blog} />
+                <TypographyIconBio
+                  icon="social"
+                  text={userDetail.twitter_username}
+                />
+              </CardContent>
+
             </Grid>
+
+            {/* REPOSITORIOS */}
+
             <Grid item xs={12} md={8}>
-              <Typography variant="h5">Repositories</Typography>
+              <Typography variant="h5" fontWeight="900">Repositorios://</Typography>
               <Box>
                 {repos.length > 0 &&
                   repos.map((repo, index) => (
                     <Box key={index} mt={3}>
                       <Card>
-                        <CardContent>
+                        {/* BOX DE CADA REPO */}
+                        <CardContent style={{
+                          backgroundColor: "#000", color: "#fff", borderRadius: "0em"
+                        }}>
                           <Typography variant="h6" fontWeight="800">
                             {repo.name}
                           </Typography>
-                          <Typography variant="body1">
+                          <Typography variant="body1" fontWeight="200"> Lenguaje://
                             {repo.language}
                           </Typography>
                         </CardContent>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SearchView, UsersView, UserView } from "../pages";
+import Users from "../pages/users/users";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SearchView />} />
-        <Route path="/users/:username" element={<UsersView />} />
+        <Route path="/users/:username" element={<Users />} />
         <Route path="/user/:username" element={<UserView />} />
       </Routes>
     </BrowserRouter>
