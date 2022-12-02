3ddd284efd3aae947de0cb1bb0ad17d32d10258a
Waldir <waliuxd@gmail.com>
2022-12-01 22:53:50 -0500

    header,fondo,y mas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
 import Router from "./router";
+import "./index.css"
+import Header from "./components/header";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <Header/>
       <Router />
     </ThemeProvider>
   );
